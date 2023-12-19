03ecbbc08b3de577d6c8e692f2188f118fcae3b2
fly-1-1 <2673024411@qq.com>
2023-12-20 07:44:24 +0800

    商品模块  图片批量上传

---
+++ gin11/controllers/admin/goods.go
@@ -0,0 +1,77 @@
+package admin
+
+import (
+	"gin11/models"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type GoodsController struct {
+	BaseController
+}
+
+func (con GoodsController) Index(c *gin.Context) {
+	c.HTML(http.StatusOK, "admin/goods/index.html", gin.H{})
+}
+func (con GoodsController) Add(c *gin.Context) {
+	//获取商品分类
+	var goodsCateList []models.GoodsCate
+	models.DB.Where("pid=0").Preload("GoodsCateItems").Find(&goodsCateList)
+
+	//获取所有颜色信息
+	var goodsColorList []models.GoodsColor
+	models.DB.Find(&goodsColorList)
+
+	//获取商品规格包装
+	var goodsTypeList []models.GoodsType
+	models.DB.Find(&goodsTypeList)
+
+	c.HTML(http.StatusOK, "admin/goods/add.html", gin.H{
+		"goodsCateList":  goodsCateList,
+		"goodsColorList": goodsColorList,
+		"goodsTypeList":  goodsTypeList,
+	})
+}
+
+func (con GoodsController) GoodsTypeAttribute(c *gin.Context) {
+	cateId, err1 := models.Int(c.Query("cateId"))
+	var goodsTypeAttributeList []models.GoodsTypeAttribute
+	err2 := models.DB.Where("cate_id = ?", cateId).Find(&goodsTypeAttributeList).Error
+	if err1 != nil || err2 != nil {
+		c.JSON(http.StatusOK, gin.H{
+			"success": false,
+			"result":  "",
+		})
+	} else {
+		c.JSON(http.StatusOK, gin.H{
+			"success": true,
+			"result":  goodsTypeAttributeList,
+		})
+	}
+}
+
+func (con GoodsController) DoAdd(c *gin.Context) {
+	attrIdList := c.PostFormArray("attr_id_list")
+	attrValueList := c.PostFormArray("attr_value_list")
+	goodsImageList := c.PostFormArray("goods_image_list")
+	c.JSON(200, gin.H{
+		"attrIdList":     attrIdList,
+		"attrValueList":  attrValueList,
+		"goodsImageList": goodsImageList,
+	})
+}
+
+func (con GoodsController) ImageUpload(c *gin.Context) {
+	//上传图片
+	imgDir, err := models.UploadImg(c, "file") //注意：可以在网络里面看到传递的参数
+	if err != nil {
+		c.JSON(http.StatusOK, gin.H{
+			"link": "",
+		})
+	} else {
+		c.JSON(http.StatusOK, gin.H{
+			"link": "/" + imgDir,
+		})
+	}
+}
---
+++ gin11/routers/defaultRouters.go
@@ -0,0 +1,19 @@
+package routers
+
+import (
+	"gin11/controllers/itying"
+
+	"github.com/gin-gonic/gin"
+)
+
+func DefaultRoutersInit(r *gin.Engine) {
+	defaultRouters := r.Group("/")
+	{
+		defaultRouters.GET("/", itying.DefaultController{}.Index)
+		defaultRouters.GET("/thumbnail1", itying.DefaultController{}.Thumbnail1)
+		defaultRouters.GET("/thumbnail2", itying.DefaultController{}.Thumbnail2)
+		defaultRouters.GET("/qrcode1", itying.DefaultController{}.Qrcode1)
+		defaultRouters.GET("/qrcode2", itying.DefaultController{}.Qrcode2)
+
+	}
+}
